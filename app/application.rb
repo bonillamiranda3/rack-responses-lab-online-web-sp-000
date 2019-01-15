ae6c46efb77b2798e078ee1a3b0dd2556a101e90
bonillamiranda3 <bonillamiranda3@gmail.com>
2019-01-15 20:44:54 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,8 +2,8 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
-    resp.write "#{time}"\n"
-    if time >=
+  
+    if Time.now.hour > 12
       resp.write "Good Afternoon!"
     else
       resp.write "Good Morning!"
